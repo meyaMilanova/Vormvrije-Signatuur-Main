1123405802ede741093bae5c206098a4e838d257
meya <milanela.milanova@gmail.com>
2025-06-21 14:35:40 +0200

    Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+    content: [
+        "./*.html", // Scans all HTML files in the root directory
+    ],
+    theme: {
+        extend: {
+            // Add your custom styles here
+            colors: {
+                orangeB: "#FF5A00"
+            },
+        },
+    },
+    plugins: [],
+};
